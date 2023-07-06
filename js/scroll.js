9aa0b9cf027f92e464b7edf928cfd15b65f8d8eb
정승혁 <jest9200@naver.com>
2023-07-06 18:09:44 +0900

    0706

--- js/scroll.js
+++ js/scroll.js
@@ -2,6 +2,27 @@ const header = document.querySelector("#header");
 let scrollTop = 0;
 let bar = document.getElementsByClassName("bar")[0];
 
+const cont1 = document.querySelector(".cont1");
+const cont2 = document.querySelector(".cont2");
+const cont3 = document.querySelector(".cont3");
+const cont4 = document.querySelector(".cont4");
+
+const contDivs = document.querySelectorAll("#container > div");
+console.log(contDivs);
+
+// gnb메뉴들 누르면 각 구간으로 스크롤이동
+const gnbMenus = document.querySelectorAll("#header .gnb li");
+for(let i=0; i<gnbMenus.length; i++){
+    gnbMenus[i].addEventListener("click",(e)=>{
+        e.preventDefault();
+        window.scrollTo({
+            top:contDivs[i].offsetTop,
+            behavior:"smooth"
+        })
+    })
+}
+
+
 window.addEventListener("scroll", () => {
     // 프로그래스 바 기능
     scrollTop = document.documentElement.scrollTop;
@@ -16,6 +37,28 @@ window.addEventListener("scroll", () => {
         header.classList.add("on");
     } else {
         header.classList.remove("on");
+    }
+
+    if(scTop >= cont1.offsetTop && scTop < cont2.offsetTop){
+        for(let i=0; i<gnbMenus.length; i++){
+            gnbMenus[i].classList.remove("on");
+        }
+        gnbMenus[0].classList.add("on");
+    } else if(scTop >= cont2.offsetTop && scTop < cont3.offsetTop){
+        for(let i=0; i<gnbMenus.length; i++){
+            gnbMenus[i].classList.remove("on");
+        }
+        gnbMenus[1].classList.add("on");
+    } else if(scTop >= cont3.offsetTop && scTop < cont4.offsetTop){
+        for(let i=0; i<gnbMenus.length; i++){
+            gnbMenus[i].classList.remove("on");
+        }
+        gnbMenus[2].classList.add("on");
+    } else if(scTop >= cont4.offsetTop){
+        for(let i=0; i<gnbMenus.length; i++){
+            gnbMenus[i].classList.remove("on");
+        }
+        gnbMenus[3].classList.add("on");
     }
 });
 
